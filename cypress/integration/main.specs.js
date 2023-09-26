7f17892307ea0bc5d9a3ebdcbca58c9cecb46c2b
puputym <puputmaulida75@gmail.com>
2023-09-26 10:18:36 +0700

    Update Fav

--- cypress/integration/main.specs.js
+++ cypress/integration/main.specs.js
@@ -5,11 +5,17 @@ describe('Running Automation Test TMDB Movie', ()=>{
     const loginPage = new LoginPage();
     const moviePage = new MoviePage();
 
+    it('should be able to failed mark favorite movie', ()=>{
+        loginPage.visit();
+        moviePage.favMovieBeforeLogin();
+        moviePage.verifyFailedFavMovie();
+    })
     beforeEach(() =>{
         loginPage.visit();
         loginPage.login();
-        loginPage.verifySuccessLogin();
+        // loginPage.verifySuccessLogin();
     })
+
     it('should be able to mark favorite movie', ()=>{
         moviePage.favoriteMovie();
         moviePage.verifyFavoriteMovie();
--- cypress/page-object/favorite-movie.js
+++ cypress/page-object/favorite-movie.js
@@ -15,7 +15,16 @@ export default class MoviePage {
     cy.contains(elementLocator.clickAvatarOpen, 'Lists').click({force: true});
     cy.get(elementLocator.clickOverview).should('be.visible').click();
     cy.contains(elementLocator.clickFavorite, 'Favorites').should('be.visible').click({force: true});
-    cy.get(elementLocator.clickMovie).should('be.visible').click();
+    cy.contains(elementLocator.ClickMovie, 'Movies').should('be.visible').click({force: true});
     cy.contains(elementLocator.verifySuccessFavMovie, 'Remove').should('be.visible');
+  }
+  favMovieBeforeLogin(){
+    cy.get(elementLocator.favMovieBeforeLogin).click();
+    cy.get(elementLocator.buttonFav).click();
+  }
+  verifyFailedFavMovie(){
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal(`Login to add this movie to your favorite list`)
+  })
   }
 }
